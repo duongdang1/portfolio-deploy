1e319a2a91ee6516c8facad582c5c7f90fc777bb
Dang Duong <duongminhvietdang@gmail.com>
2021-03-04 10:57:16 +0700

    update

--- src/profiles/works/components/WorkItem.js
+++ src/profiles/works/components/WorkItem.js
@@ -10,7 +10,32 @@ const WorkItem = (props) => {
           <div className="title-style">
             <h1>{props.title}</h1>
           </div>
-          <TabProject/>
+          <div className="work-tab-item">
+          
+            <div className="first-work">
+              <h1>Student Technician at ITS - Learning Environment, Union College</h1>
+              <li> - Set up and manage technology devices for on campus events. </li>
+              <li> - Assist students and faculties with issues related to technology around campus. </li>
+              <li> - Plan and perform multiple time-critical tasks</li>
+              <li> - Maintaining equipment in campus storage caches</li>
+            </div>
+            <div className="second-work">
+              <h1>Student Technician at Ustar, Union College</h1>
+              <li> - Diagnose and assist with correction of operating system and general software problems including, but not limited to corrupt files, viruses, adware, etc.</li>
+              <li> - Assist with general on-campus network connection issues</li>
+              <li> - Some hardware diagnostics and minor repairs</li>
+            </div>
+            <div className="third-work">
+              <h1>Teacher at mindX</h1>
+              <li> - Teach Computer Science to highschool student </li>
+              <li> - Class I teach:</li>
+              <li> - C4T - Python: Students learn the fundamentals of programming through Python. Students will then develop their own application.</li>  
+            </div>
+         
+        </div>
+          <div className="tab">
+            <TabProject/>
+          </div>
         </div>
       </Zoom>
     </div>
